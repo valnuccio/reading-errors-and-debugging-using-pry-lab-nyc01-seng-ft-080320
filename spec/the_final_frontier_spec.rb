1489ddf2ad48a773c59b4f6217294d031d32aece
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-24 22:00:35 +0000

    Automatically backed up by Learn

--- spec/the_final_frontier_spec.rb
+++ spec/the_final_frontier_spec.rb
@@ -2,7 +2,7 @@ require_relative '../fix_using_tests/the_final_frontier'
 
 describe "the_final_frontier" do
   it "`engage` should return the captain's log and crew greetings" do
-    expect(engage).to output(/Captain's Log, star date /).to_stdout
+    expect [engage].to output(/Captain's Log, star date /).to_stdout
   end
 end
 
